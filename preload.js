e5088f435ff251b86b8e95470acb807f42c0e9f3
Myles Lake <myles52@live.ca>
2022-04-30 10:03:38 -0400

    Added basic file conversion.

--- preload.js
+++ preload.js
@@ -1,27 +1,49 @@
 const ipc = require("electron").ipcRenderer;
 const fs = require("fs");
+const jimp = require('jimp');
 const imageCompression = require("browser-image-compression");
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("file_upload").addEventListener("change", (e) => {});
+  document.getElementById("file_upload").addEventListener("change", (e) => { });
 
   document.getElementById("submit").addEventListener("click", async (_) => {
     for (const file of document.getElementById("file_upload").files) {
+      console.log('file', file);
       const options = {
         maxSizeMB: 1,
         maxWidthOrHeight: 1920,
         useWebWorker: false,
       };
       try {
-        const c = await imageCompression(file, options);
+        const compressed = await imageCompression(file, options);
         const buffer = Buffer.from(await c.arrayBuffer());
-        if (!fs.existsSync('compressedImages')){
+        if (!fs.existsSync('compressedImages')) {
           fs.mkdirSync('compressedImages');
         }
         fs.writeFile(`compressedImages/${compressed.name}`, buffer, () => console.log("image saved"));
       } catch (err) {
         console.log(err);
       }
+    }
+  });
+
+  /**
+   * Converts images to desired type.
+   */
+  document.getElementById('convert').addEventListener('click', (_) => {
+    if (!fs.existsSync('convertedImages')) {
+      fs.mkdirSync('convertedImages');
+    }
+
+    const fileType = document.getElementById('file-type').value;
+
+    for (const file of document.getElementById('file_upload').files) {
+      jimp.read(file.path, (err, convert) => {
+        if (err) console.log(err);
+
+        let filename = file.name.split('.')[0];
+        convert.write(`convertedImages/${filename}.${fileType}`);
+      });
     }
   });
 });
